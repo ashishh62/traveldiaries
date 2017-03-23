7f66bbad6a20cdc645ea9d9ce527f8f9005a5a15
ashishh62 <ashishh62@gmail.com>
2017-03-24 02:01:41 +0530

    added UserDetailsRepository interface

---
+++ src/main/java/com/traveldiaries/repositories/UserDetailsRepository.java
@@ -0,0 +1,8 @@
+package com.traveldiaries.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserDetailsRepository extends MongoRepository{
+	
+	
+}
